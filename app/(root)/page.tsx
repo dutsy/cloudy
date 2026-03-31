e4862c57903dc1be880c13bd87ae48c82c2d679f
Qussay <qussayf@gmail.com>
2026-03-31 16:40:26 +0300

    feat: initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Home",
+};
+
+
 const Homepage = () => {
   return <>Prostore</>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import '@/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Cloudy",
-  description: "Our modern Coffee Shop",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -16,9 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en">
+      lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
-        {children} 
+        <ThemeProvider attribute='class' defaultTheme="light" enableSystem disableTransitionOnChange >
+        {children}
+        </ThemeProvider>
       </body>
     </html>
   );

3abd8b24e25ec5601140ba715a0609a6221e322c
Qussay Firon <37940638+dutsy@users.noreply.github.com>
2026-07-23 01:50:40 +0300

    Update hostname for remote image patterns

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'kbslzdttdaazraekbrfp.supabase.co',
+        hostname: 'tbbwppqtevvozqngsejp.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
